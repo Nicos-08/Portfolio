61b753f783f940f68a077d4c0a20086d46e00d79
Nicos-08 <thierrylemoine136@gmail.com>
2023-04-19 18:29:36 +0200

    Configuration des fichiers de base

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="App">
+			<h1 style={{ fontFamily: 'chillax' }}>Le reeeeeve</h1>
+		</div>
+	);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//Pages
 import App from './App';
+
+//Styles
+import './styles/base/default.css';
+import './styles/base/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
