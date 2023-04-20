8fb0e1e07824933b009555227a984942e33eb8b9
Nicos-08 <thierrylemoine136@gmail.com>
2023-04-20 19:02:44 +0200

    Création des fichiers à duppliquer

---
+++ src/a_recopier/composant.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import "../styles/components/composant/composant.css";
+
+const Composant = () => {
+	return (
+        <div className="composant">
+            
+        </div>
+    );
+};
+
+Composant.propTypes = {};
+
+Composant.defaultProps = {};
