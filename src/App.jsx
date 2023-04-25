d9a5622b3b23b7ffbddd14c52339f7d577080cc0
Nicos-08 <thierrylemoine136@gmail.com>
2023-04-25 19:36:30 +0200

    Composant LiensMenu terminé

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import LiensItems from "./components/navigation/liens_items/liens_items";
+import LiensMenu from "./components/navigation/liens_menu/liens_menu";
 import "./styles/base/fonts.css";
 import "./styles/base/default.css";
 
 function App() {
 	return (
 		<div className="App">
-			<LiensItems theme="clair" nombre="2" texte="Creations" />
+			<LiensMenu theme="clair" />
 		</div>
 	);
 }
---
+++ src/components/navigation/liens_menu/liens_menu.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import "../../../styles/components/navigation/liens_menu/liens_menu.css";
+import LiensItems from "../liens_items/liens_items";
+
+const LiensMenu = ({ theme }) => {
+	return (
+		<div className="liens_menu">
+			<LiensItems theme={theme} nombre="1" texte="Accueil" />
+			<LiensItems theme={theme} nombre="2" texte="Creations" />
+			<LiensItems theme={theme} nombre="3" texte="Competences" />
+			<LiensItems theme={theme} nombre="4" texte="Contact" />
+		</div>
+	);
+};
+
+LiensMenu.propTypes = {
+	theme: PropTypes.string.isRequired,
+};
+
+LiensMenu.defaultProps = {};
+
+export default LiensMenu;
