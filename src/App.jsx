3907705d2a76f893d0b313c05b35e20f2940f939
Nicos-08 <thierrylemoine136@gmail.com>
2023-04-25 19:09:42 +0200

    Composant LiensItems terminé

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import MenuDeNavigation from "./components/navigation/menu_de_navigation/menu_de_navigation";
+import LiensItems from "./components/navigation/liens_items/liens_items";
 import "./styles/base/fonts.css";
 import "./styles/base/default.css";
 
 function App() {
 	return (
 		<div className="App">
-			<MenuDeNavigation
-				theme="sombre"
-				arborescence={["Page de projet", "Choixpeau magique"]}
-			/>
+			<LiensItems theme="clair" nombre="2" texte="Creations" />
 		</div>
 	);
 }
---
+++ src/components/navigation/liens_items/liens_items.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import "../../../styles/components/navigation/liens_items/liens_items.css";
+
+const LiensItems = ({ theme, nombre, texte }) => {
+	return (
+		<div className="liens_items">
+			<span className={`liens_items__numero liens_items--${theme}`}>
+				{`0${nombre}`}
+			</span>
+			<span className={`liens_items__texte liens_items--${theme}`}>
+				{texte}
+			</span>
+		</div>
+	);
+};
+
+LiensItems.propTypes = {
+	theme: PropTypes.string.isRequired,
+	nombre: PropTypes.string.isRequired,
+	texte: PropTypes.string.isRequired,
+};
+
+LiensItems.defaultProps = {};
+
+export default LiensItems;
