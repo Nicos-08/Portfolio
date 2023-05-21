8acbdd7dea26006ea32ebfe94f555810905e0282
Nicos-08 <thierrylemoine136@gmail.com>
2023-05-21 18:01:05 +0200

    Composant Competence terminé (sauf transitions)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./styles/base/fonts.css";
 import "./styles/base/default.css";
 import creations from "./data/creations";
-import Visualisateur from "./components/projets/visualisateur/visualisateur";
+import Competence from "./components/competences/competence";
+import competences from "./data/competences";
 
 function App() {
 	return (
 		<div className="App">
-			<Visualisateur projet={creations[1]} />
+			<Competence theme="sombre" competence={competences[6]} />
 		</div>
 	);
 }
---
+++ src/components/competences/competence.jsx
@@ -0,0 +1,55 @@
+import PropTypes from "prop-types";
+import "../../styles/components/competences/competence.css";
+import FlecheBasIcon from "../icons/fleche_bas/fleche_bas_icon";
+import { useState } from "react";
+
+const Competence = ({ theme, competence }) => {
+	const [ouvert, setOuvert] = useState(false);
+	return (
+		<div
+			className={`competence competence--${theme}`}
+			onClick={() => setOuvert(!ouvert)}
+		>
+			<div className="competence__partie_visible">
+				<span
+					className={`competence__partie_visible__nom competence__partie_visible__nom--${theme}`}
+				>
+					{competence.nom}
+				</span>
+				{ouvert ? (
+					<div
+						className="competence__partie_visible__fleche_bas_icon"
+						style={{ transform: "rotate(180deg)" }}
+					>
+						<FlecheBasIcon theme={theme === "sombre" ? "clair" : "sombre"} />
+					</div>
+				) : (
+					<div className="competence__partie_visible__fleche_bas_icon">
+						<FlecheBasIcon theme={theme === "sombre" ? "clair" : "sombre"} />
+					</div>
+				)}
+			</div>
+			{ouvert && (
+				<ul className="competence__partie_invisible">
+					{competence.details.map((competence, index) => (
+						<li
+							key={`${competence}-${index}`}
+							className={`competence__partie_invisible__element competence__partie_invisible__element--${theme}`}
+						>
+							{competence}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+};
+
+Competence.propTypes = {
+	theme: PropTypes.string.isRequired,
+	competence: PropTypes.object.isRequired,
+};
+
+Competence.defaultProps = {};
+
+export default Competence;
